269cc57494f8d535817ae9d54b29bd05fbc0a43d
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-01-08 15:25:31 +0530

    hello

--- billl.py
+++ billl.py
@@ -1,1 +1,8 @@
+def calculate_bill(units):
+    # Initialize charges
+    first_charge = second_charge = remaining_charge = 0
 
+    # First 100 units → ₹2/unit
+    if units > 0:
+        first_units = min(units, 100)
+        first_charge = first_units * 2
